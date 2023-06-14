5ba058ff9a1dba524e05a4865b69bf5de13b1402
jihandong <jihandong@xiaomi.com>
2023-06-14 12:11:27 +0800

    perceptron: training and testing done
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,56 +1,79 @@
-import pandas as pd
-import numpy as np
 from sklearn.datasets import load_iris
+import numpy as np
+import pandas as pd
+import time
+
 
 class Perceptron:
-    def __init__(self):
-        self.__load(0.7) # percentage of training data.
-        self.stride = 0.1 # learning rate.
-        self.w = np.random.rand(self.train_x.shape[1], dtype=np.float32)
-        self.b = 0
-
-    def __load(self, p):
-        # 1. Prepare data for perceptron.
-        iris = load_iris()
-        x = iris.data
-        y = iris.target
-        y = np.where(iris.target > 0, 1, -1)
-
-        # 2. Shuffle the x,y in the same order.
+    def __init__(self, x, y, p, stride):
+        # Shuffle the x,y in the same order.
         order = np.random.permutation(x.shape[0])
         x = x[order]
         y = y[order]
 
-        # 3. Split for training and testing.
+        # Split data for training and testing.
         idx = int(p * x.shape[0])
         self.train_x = x[:idx]
         self.train_y = y[:idx]
         self.test_x = x[idx:]
         self.test_y = y[idx:]
 
-    def __predict(self):
-        predict_y = np.dot(self.w, self.train_x) + self.b
-        predict_y = np.where(predict_y > 0, 1, -1)
-        result = self.train_y * predict_y
-        return result
+        # Configure with hyper-parameters.
+        self.configure(np.random.rand(self.train_x.shape[1]), 0, stride)
 
-    def __update(self, idx):
-        w = self.w
-        b = self.b
-        self.w = w + self.stride * self.train_y[idx] * self.train_x[idx]
-        self.b = b + self.stride * self.train_y[idx]
+    def configure(self, w, b, stride):
+        self.w = w
+        self.b = b
+        self.stride = stride
+
+    def perceive(self, x, y):
+        # Apply classify function.
+        predict_y = np.dot(x, self.w) + self.b
+        predict_y = np.where(predict_y > 0, 1, -1)
+
+        # Get mis-classify entries.
+        result = y * predict_y
+        error_entries = np.where(result < 0)[0]
+        return error_entries
 
     def train(self):
+        start_time = time.perf_counter()
+        round = 0
+
+        # Stochastic Gradient Descent
         while (1):
-            # 1. Apply w,b and pick an mis-classified sample.
-            result = self.__predict()
-            error_result = np.where(result < 0)[0]
-            if (error_result.size > 0):
-                index = np.random.choice(error_result)
-                self.__update(index)
+            round = round + 1
+            error_entries = self.perceive(self.train_x, self.train_y)
+            if (error_entries.size > 0):
+                idx = np.random.choice(error_entries)
+                # Update paramters.
+                self.w = self.w + self.stride * self.train_y[idx] * self.train_x[idx]
+                self.b = self.b + self.stride * self.train_y[idx]
             else:
                 break
-        print()
+
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print("Training takes %dr, costs %fs" % (round, run_time))
 
     def test(self):
+        error_entries = self.perceive(self.test_x, self.test_y)
+        accuracy = 1.0 - 1.0 * error_entries.size / self.test_x.shape[0]
+        print("Testing accuracy = %f" % accuracy)
+
+    def show(self):
         pass
+
+
+if __name__ == '__main__':
+    # Prepare iris data.
+    iris = load_iris()
+    x = iris.data
+    y = iris.target
+    y = np.where(iris.target > 0, 1, -1)
+
+    # Apply classic perceptron model.
+    perceptron = Perceptron(x, y, 0.7, 0.1)
+    perceptron.train()
+    perceptron.test()
+    perceptron.show()
